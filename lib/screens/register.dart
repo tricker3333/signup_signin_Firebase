f56ce3fb8c087a4c70c17dab5f1bbda28728e284
YashHere <tricker3333@gmail.com>
2023-11-10 16:27:20 +0530

    gfhfg

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:simple_login/screens/home.dart';
-import 'package:simple_login/screens/loginpg.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({super.key});
